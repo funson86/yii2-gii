f7cf93082af42a0fe3cfaa07842aae26166b1bc2
Funson Lee <funson86@gmail.com>
2014-10-22 17:39:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,64 @@
+Yii2-gii for Chinese
+========
+主要针对中文的优化
+CRUD
+1.减少翻译标签，将Create Post分开成Create和Post，翻译文件的标签大大减少
+2.针对Adminlte减少h1标签
+3.针对鉴权提供can函数，默认注释
+
+Model
+1.增加beforeSave和afterSave两个函数，默认修改create_time和update_time
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist funson86/yii2-gii "*"
+```
+
+or add
+
+```
+"funson86/yii2-gii": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+在main-local.php中配置：
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*', '192.168.178.20'],
+        'generators' => [ //here
+            'crud' => [ //name generator
+                'class' => 'yii\gii\generators\crud\Generator', //class generator
+                'templates' => [ //setting for out templates
+                    'funson86' => '@vendor/funson86/yii2-gii/crud/default', //name template => path to template
+                ]
+            ],
+            'model' => [ //name generator
+                'class' => 'yii\gii\generators\model\Generator', //class generator
+                'templates' => [ //setting for out templates
+                    'funson86' => '@vendor/funson86/yii2-gii/model/default', //name template => path to template
+                ]
+            ],
+        ],
+    ];
+}
+
+在gii新建模板时会多出一个选项。
---
+++ model/default/model.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * This is the template for generating the model class of a specified table.
+ */
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\model\Generator */
+/* @var $tableName string full table name */
+/* @var $className string class name */
+/* @var $tableSchema yii\db\TableSchema */
+/* @var $labels string[] list of attribute labels (name => label) */
+/* @var $rules string[] list of validation rules */
+/* @var $relations array list of relations (name => relation declaration) */
+
+echo "<?php\n";
+?>
+
+namespace <?= $generator->ns ?>;
+
+use Yii;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "<?= $generator->generateTableName($tableName) ?>".
+ *
+<?php foreach ($tableSchema->columns as $column): ?>
+ * @property <?= "{$column->phpType} \${$column->name}\n" ?>
+<?php endforeach; ?>
+<?php if (!empty($relations)): ?>
+ *
+<?php foreach ($relations as $name => $relation): ?>
+ * @property <?= $relation[1] . ($relation[2] ? '[]' : '') . ' $' . lcfirst($name) . "\n" ?>
+<?php endforeach; ?>
+<?php endif; ?>
+ */
+class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . "\n" ?>
+{
+    const STATUS_INACTIVE = 0;
+    const STATUS_ACTIVE = 1;
+    const STATUS_DELETED = -1;
+    private $_statusLabel;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '<?= $generator->generateTableName($tableName) ?>';
+    }
+<?php if ($generator->db !== 'db'): ?>
+
+    /**
+     * @return \yii\db\Connection the database connection used by this AR class.
+     */
+    public static function getDb()
+    {
+        return Yii::$app->get('<?= $generator->db ?>');
+    }
+<?php endif; ?>
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [<?= "\n            " . implode(",\n            ", $rules) . "\n        " ?>];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+<?php foreach ($labels as $name => $label): ?>
+            <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
+<?php endforeach; ?>
+        ];
+    }
+<?php foreach ($relations as $name => $relation): ?>
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function get<?= $name ?>()
+    {
+        <?= $relation[0] . "\n" ?>
+    }
+<?php endforeach; ?>
+
+    /**
+     * Before save.
+     * create_time update_time
+     */
+    public function beforeSave($insert)
+    {
+        if(parent::beforeSave($insert))
+        {
+            if($this->isNewRecord)
+            {
+                $this->create_time = new Expression('NOW()');
+                $this->update_time = $this->create_time;
+                $this->create_user_id = Yii::$app->getUser()->id;
+            }
+            else
+            {
+                $this->update_time = new Expression('NOW()');
+                $this->update_user_id = Yii::$app->getUser()->id;
+            }
+            return true;
+        }
+        else
+            return false;
+    }
+
+    /**
+     * After save.
+     *
+     */
+    /*public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+        // add your code here
+    }*/
+
+    /**
+     * @inheritdoc
+     */
+    public static function getArrayStatus()
+    {
+        return [
+            self::STATUS_INACTIVE => Yii::t('app', 'STATUS_INACTIVE'),
+            self::STATUS_ACTIVE => Yii::t('app', 'STATUS_ACTIVE'),
+            self::STATUS_DELETED => Yii::t('app', 'STATUS_DELETED'),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getStatusLabel()
+    {
+        if ($this->_statusLabel === null) 
+        {
+            $statuses = self::getArrayStatus();
+            $this->_statusLabel = $statuses[$this->status];
+        }
+        return $this->_statusLabel;
+    }
+
+
+}
